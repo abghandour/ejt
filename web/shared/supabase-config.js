7ea30f4cf8b45a5ade4b86adf8f74ab9f21d850f
Ahmed Ghandour <abghandour@Mac-mini.local>
2026-03-16 17:03:30 -0400

    update url

--- web/shared/supabase-config.js
+++ web/shared/supabase-config.js
@@ -1,4 +1,4 @@
 var SUPABASE_URL = 'https://yegbudbzbsnrwcjtygro.supabase.co';
 var SUPABASE_ANON_KEY = 'sb_publishable_3Gm24GXAfwQIGdz4UG3FBQ_MpFB438U';
 var PATREON_CLIENT_ID = 'W42JrvHw2wHY6Z7_BpShj6Y2wGdEb_-ZRHCD7Z_ea4-ozSY-N2_sYuoT-a32sg19';
-var PATREON_REDIRECT_URI = window.location.origin + '/mobile/index.html';
+var PATREON_REDIRECT_URI = window.location.origin + '/ejt/web/mobile/index.html';
